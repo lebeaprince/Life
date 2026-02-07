cd7a743347ca5ff6fa78d5a310e92e2eba2390e2
Phetole <lebeaprince@gmail.com>
2026-02-07 18:56:15 +0200

    tenant repo fix

--- backend/identity-service/src/main/java/com/nimbus/identity/repository/TenantRepository.java
+++ backend/identity-service/src/main/java/com/nimbus/identity/repository/TenantRepository.java
@@ -2,6 +2,8 @@ package com.nimbus.identity.repository;
 
 import com.nimbus.identity.model.Tenant;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TenantRepository extends JpaRepository<Tenant, String> {
